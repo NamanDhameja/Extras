0de1fc7f1ce2b52583d2db06dc9e4b3ac3222dfd
NamanDhameja <42289613+NamanDhameja@users.noreply.github.com>
2020-10-13 12:53:34 +0530

    Merge the Tools_Hackerank

---
+++ Merge the tools.py
@@ -0,0 +1,11 @@
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        str = string[i:i+k];
+        sub = ''
+        for s in str:
+            if s not in sub:
+                sub += s;
+        print(sub);
+
+s,k=input(),int(input())
+merge_the_tools(s,k)
